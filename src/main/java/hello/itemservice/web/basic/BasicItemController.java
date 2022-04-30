973485ba46ba9900b892227e137bf7986ab1747d
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-04-30 11:44:53 +0900

    상품 상세 폼 - 상품 등록 폼: GET /basic/items/add, 상품 등록 처리 POST: /basic/items/add

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -35,6 +35,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "xxxx";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
